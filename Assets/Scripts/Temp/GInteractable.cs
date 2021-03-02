5ba66b077033757c4bf5f4c04622bc1b776a28fb
Minstreams <32974920+Minstreams@users.noreply.github.com>
2021-03-02 05:13:44 +0000

    Collectable Crystals

---
+++ Assets/Scripts/Temp/GInteractable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSystem;
+
+public class GInteractable : MonoBehaviour
+{
+    [Label] public TextMesh tipTextMesh;
+    [Label] public string tipText;
+    [Label] public SimpleEvent onInteracted;
+
+    bool pendingInteract;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            pendingInteract = true;
+            tipTextMesh.text = $"[{InputSystem.Setting.MainKeys[InputKey.Action].ToString()}] {tipText}";
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            pendingInteract = false;
+            tipTextMesh.text = null;
+        }
+    }
+    protected virtual void Start()
+    {
+        tipTextMesh.text = null;
+    }
+
+    void Update()
+    {
+        if (pendingInteract && InputSystem.GetKeyDown(InputKey.Action))
+        {
+            OnInteracted();
+        }
+    }
+
+    protected virtual void OnInteracted()
+    {
+        onInteracted?.Invoke();
+    }
+}
