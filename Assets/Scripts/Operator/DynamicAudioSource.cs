741bafd6eebae49594b2e965cf3cffe9df502f83
Minstreams <32974920+Minstreams@users.noreply.github.com>
2021-04-27 15:03:38 +0100

    Footsteps Effect

--- Assets/Scripts/Operator/DynamicAudioSource.cs
+++ Assets/Scripts/Operator/DynamicAudioSource.cs
@@ -19,11 +19,19 @@ namespace GameSystem.Operator
         [Label] public Vector2 pitchRange = Vector2.one;
         [Label] public Vector2 volumeRange = Vector2.up;
 
+        [Label] public AudioClip[] clips;
+
         public void Play(float input)
         {
             float t = Mathf.InverseLerp(inputRange.x, inputRange.y, input);
             aus.pitch = Mathf.Lerp(pitchRange.x, pitchRange.y, t);
             aus.volume = Mathf.Lerp(volumeRange.x, volumeRange.y, t);
+            if (clips != null && clips.Length > 0) aus.clip = clips[Random.Range(0, clips.Length)];
+            aus.Play();
+        }
+        public void Play()
+        {
+            if (clips != null && clips.Length > 0) aus.clip = clips[Random.Range(0, clips.Length)];
             aus.Play();
         }
 
